55cc83abb76dfd7be38706054c34bb781b71f041
michael <michaelcollins1998@hotmail.co.uk>
2017-06-08 11:49:21 +0100

    finished :)

--- src/main/java/com/pbs/model/NoAction.java
+++ src/main/java/com/pbs/model/NoAction.java
@@ -7,7 +7,7 @@ import java.util.Date;
 /**
  * Created by Michael on 09/05/2017.
  */
-public class NoAction extends Report{
+public class NoAction extends Report {
 
     private String suspectedCrimes;
     private String suspects;
@@ -17,10 +17,11 @@ public class NoAction extends Report{
     private String reportType = "no_action";
 
     // This constructor has to be implemented due to weird Spring implementation
-    public NoAction() { }
+    public NoAction() {
+    }
 
     public NoAction(Integer id, String reportName, String reportDate, Integer officerId, String suspectedCrimes, String suspects, String officers, String statement, String details, String isVerified) {
-        super(id, reportName, reportDate, officerId, isVerified,"no_action");
+        super(id, reportName, reportDate, officerId, isVerified, "no_action");
         this.suspectedCrimes = suspectedCrimes;
         this.suspects = suspects;
         this.officers = officers;
--- src/main/java/com/pbs/model/Report.java
+++ src/main/java/com/pbs/model/Report.java
@@ -4,15 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public abstract class Report {
-	private Integer id;
-	private String reportName = "";
-	private String reportDate= "";
-	private Integer officerId;
-	private String isVerified= "";
-	private String reportType;
-	Map<String,String> furtherDetails = new HashMap<String, String>();
+    private Integer id;
+    private String reportName = "";
+    private String reportDate = "";
+    private Integer officerId;
+    private String isVerified = "";
+    private String reportType;
+    Map<String, String> furtherDetails = new HashMap<String, String>();
 
-	public Report() { }
+    public Report() {
+    }
 
     public Report(Integer id, String reportName, String reportDate, Integer officerId, String isVerified, String reportType) {
         this.id = id;
@@ -23,43 +24,43 @@ public abstract class Report {
         this.reportType = reportType;
     }
 
-	/**
-	 * This is an abstract method that is used by each child class
+    /**
+     * This is an abstract method that is used by each child class
      * It allows child classes to populate the details specific to their type, and in a format that is readable
      * on the front end
-	 */
-	abstract void populateFurtherDetails();
+     */
+    abstract void populateFurtherDetails();
 
-	//This method is needed to load required data, it is used but is abstracted
-	public Map<String, String> getFurtherDetails() {
-		return furtherDetails;
-	}
+    //This method is needed to load required data, it is used but is abstracted
+    public Map<String, String> getFurtherDetails() {
+        return furtherDetails;
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getReportName() {
-		return reportName;
-	}
+    public String getReportName() {
+        return reportName;
+    }
 
-	public String getReportDate() {
-		return reportDate;
-	}
+    public String getReportDate() {
+        return reportDate;
+    }
 
-	public Integer getOfficerId() {
-		return officerId;
-	}
+    public Integer getOfficerId() {
+        return officerId;
+    }
 
-	public String getReportType() {
-		return reportType;
-	}
+    public String getReportType() {
+        return reportType;
+    }
 
-	public String getIsVerified() {
+    public String getIsVerified() {
         return isVerified;
     }
 
